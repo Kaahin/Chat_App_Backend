8b7a4c56291dd1472757b95ad2f3268b858e41a6
Abdirahman Kaahin <abdi_9200@hotmail.com>
2022-02-23 10:34:49 +0100

    Connected backend with mongoose

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 // LOAD MODULE
-// env
+// dotenv
 require("dotenv").config();
 // express
 const express = require("express");
@@ -8,10 +8,19 @@ const { createServer } = require("http");
 const { Server } = require("socket.io");
 // mongodb
 const { MongoClient } = require("mongodb");
+// mongoose
+const mongoose = require("mongoose");
 // openai
 const { Configuration, OpenAIApi } = require("openai");
 // nanoid
 const { v4: uuidv4 } = require("uuid");
+// schema
+const User = require("./model/User");
+// bcryptjs
+const bcrypt = require("bcryptjs");
+// jsonwebtoken
+const jwt = require("jsonwebtoken"); // importerar jsonwebtoken
+const req = require("express/lib/request");
 
 // CREATE INSTANCES
 // env
@@ -26,10 +35,21 @@ const io = new Server(httpServer, {
   },
 });
 
+// CONFIGURATION
+const Key = process.env.OPENAI.toString();
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: Key,
 });
+// openai
 const openai = new OpenAIApi(configuration);
+// mongoose
+mongoose.connect(
+  process.env.DB_CONNECT,
+  { useUnifiedTopology: true, UseNewUrlParser: true },
+  () => {
+    console.log("Connected to database!");
+  }
+);
 
 // BROADCASTING
 httpServer.listen(PORT, () => {
@@ -39,12 +59,73 @@ httpServer.listen(PORT, () => {
 io.on("connection", (socket) => {
   console.log("new client connected");
 
+  // Sign in request from client
+  socket.on("signin", async ({ first, email, password }, reply) => {
+    // Check if email exists in database
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      io.emit("signin-reply", { error: "Email is not found" });
+    }
+
+    // Check if password is correct
+    const validPassword = await bcrypt.compare(password, user.password);
+
+    if (!validPassword) {
+      io.emit("signin-reply", { error: "Invalid password" });
+    }
+
+    // Create and assign token.
+    const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
+
+    io.emit("signin-reply", { "auth-token": token });
+  });
+
+  // Sign up request from client
+  socket.on("signup", async ({ first, last, email, password }) => {
+    console.log(first);
+    // Check if user exists already
+    const emailExist = await User.findOne({ email });
+
+    if (emailExist) {
+      io.emit("signup-reply", {
+        error: ` User with an email: ${email} already exists`,
+      });
+    }
+
+    // Hash Password
+    const salt = await bcrypt.genSalt(10); // Här skapar vi en algoritm för hur säker vår lösen ska vara
+    const hashPassword = await bcrypt.hash(password, salt);
+
+    // Create new user!
+    const user = new User({
+      first,
+      last,
+      email,
+      hashPassword,
+    });
+
+    console.log(user);
+    try {
+      // Save user to database
+      await user.save();
+      console.log(user);
+      // Jwt signing inorder get token
+      const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
+      console.log(user._id);
+      io.emit("signup-reply", { user: user._id, token });
+    } catch (error) {
+      io.emit("signup-reply", error);
+    }
+  });
+
   // Receives message from client
   socket.on("request", async ({ _id, createdAt, text, user }) => {
     console.log(_id, createdAt, text, user);
 
     // Text from incoming request message as input for GPT3
     const input = text;
+
     const response = await openai.createCompletion("text-davinci-001", {
       prompt: input,
       temperature: 0.9,
